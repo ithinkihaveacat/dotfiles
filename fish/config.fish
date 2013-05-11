4a7856d78f804aaa302bf7d1400b343e0df6b23a
Michael Stillwell <mjs@beebo.org>
2013-05-11 23:27:09 +0100

    Remove greeting

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 function dir
   ls -l $argv
-end  
+end
+
+# Remove greeting
+set fish_greeting
 
 # https://github.com/fish-shell/fish-shell/blob/master/share/functions/__fish_git_prompt.fish
 set -g __fish_git_prompt_showupstream "auto"
