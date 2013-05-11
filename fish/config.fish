9367f91f1676f13cd098ac2d2d873b77917ec5a2
Michael Stillwell <mjs@beebo.org>
2013-05-11 21:36:50 +0100

    Add fish configuration

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+alias dir="ls -l"
+
+# https://github.com/fish-shell/fish-shell/blob/master/share/functions/__fish_git_prompt.fish
+set -g __fish_git_prompt_showupstream "auto"
+set -g __fish_git_prompt_showstashstate "1"
+set -g __fish_git_prompt_showdirtystate "1"
