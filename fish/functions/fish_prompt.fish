cc2d54143e84517f24154fa731cee48b2f0cc342
Michael Stillwell <mjs@beebo.org>
2013-05-14 11:14:52 +0100

    Tabs -> spaces

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,52 +1,52 @@
 function fish_prompt --description 'Write out the prompt'
-	
-	set -l last_status $status
+    
+    set -l last_status $status
 
-	# Just calculate these once, to save a few cycles when displaying the prompt
-	if not set -q __fish_prompt_hostname
-		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	end
+    # Just calculate these once, to save a few cycles when displaying the prompt
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    end
 
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
-	end
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
 
-	set -l delim '$'
+    set -l delim '$'
 
-	switch $USER
+    switch $USER
 
-	case root
+    case root
 
-		if not set -q __fish_prompt_cwd
-			if set -q fish_color_cwd_root
-				set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-			else
-				set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-			end
-		end
+        if not set -q __fish_prompt_cwd
+            if set -q fish_color_cwd_root
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+            else
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+            end
+        end
 
-	case '*'
+    case '*'
 
-		if not set -q __fish_prompt_cwd
-			set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-		end
+        if not set -q __fish_prompt_cwd
+            set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+        end
 
-	end
+    end
 
-	set -l prompt_status
-	if test $last_status -ne 0
-		if not set -q __fish_prompt_status
-			set -g __fish_prompt_status (set_color $fish_color_status)
-		end
-		set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
-	end
+    set -l prompt_status
+    if test $last_status -ne 0
+        if not set -q __fish_prompt_status
+            set -g __fish_prompt_status (set_color $fish_color_status)
+        end
+        set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
+    end
 
-	if not set -q __fish_prompt_user
-		set -g __fish_prompt_user (set_color $fish_color_user)
-	end
-	if not set -q __fish_prompt_host
-		set -g __fish_prompt_host (set_color $fish_color_host)
-	end
+    if not set -q __fish_prompt_user
+        set -g __fish_prompt_user (set_color $fish_color_user)
+    end
+    if not set -q __fish_prompt_host
+        set -g __fish_prompt_host (set_color $fish_color_host)
+    end
 
-	echo -n -s "$__fish_prompt_normal" "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ':' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt "#%s") "$__fish_prompt_normal" '$ '
+    echo -n -s "$__fish_prompt_normal" "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ':' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt "#%s") "$__fish_prompt_normal" '$ '
 end
