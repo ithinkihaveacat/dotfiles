6f795413b732cfaada604ce91c85a618d0bad984
Michael Stillwell <mjs@beebo.org>
2013-08-18 10:53:34 +0100

    Add ~/.dotfiles/bin to $PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,26 @@
 # -*- sh -*-
 
-if status --is-login
-  set -x PATH $CONFIGROOT/bin $PATH
-end
+# tool config
 
-set -x GITROOT "git@github.com:ithinkihaveacat"
-set -x GIT_COMPOSER_STALE "warn"
 set -x GREP_OPTIONS "--exclude-dir=.svn --exclude-dir=.git --binary-files=without-match"
 set -x LESS "-XMcifR"
 set -x TZ "Europe/London"
 
+# personal config
+
+set -x GITROOT "git@github.com:ithinkihaveacat"
+set -x GIT_COMPOSER_STALE "warn"
+
+# fish config
+
 set -g CDPATH . ~ ~/workspace
 
-# Remove greeting
+# http://fishshell.com/docs/2.0/#variables-special
+set fish_user_paths $HOME/.config/fish/../bin
+
+# http://fishshell.com/docs/2.0/#variables-special
 set fish_greeting
 
-# Configure git prompt
 # https://github.com/fish-shell/fish-shell/blob/master/share/functions/__fish_git_prompt.fish
 set -g __fish_git_prompt_showupstream "auto"
 set -g __fish_git_prompt_showstashstate "1"
