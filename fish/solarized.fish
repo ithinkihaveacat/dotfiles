ccb26600a62efbee21b228330e365a21179462dc
Michael Stillwell <mjs@beebo.org>
2014-05-16 16:21:00 +0100

    Improve solarized.fish docs

--- fish/solarized.fish
+++ fish/solarized.fish
@@ -1,28 +1,8 @@
 # http://ethanschoonover.com/solarized#the-values
 
-# Use the hex_* variables if terminal supports term256 and terminal colours
-# *haven't* been configured to match Solarized.  i.e. if "blue" is the default
-# blue, not Solarized blue.
-
-set -l base03  002b36
-set -l base02  073642
-set -l base01  586e75
-set -l base00  657b83
-set -l base0   839496
-set -l base1   93a1a1
-set -l base2   eee8d5
-set -l base3   fdf6e3
-set -l yellow  b58900
-set -l orange  cb4b16
-set -l red     dc322f
-set -l magenta d33682
-set -l violet  6c71c4
-set -l blue    268bd2
-set -l cyan    2aa198
-set -l green   859900
-
-# Use the term_* variables if the terminal has been configured for Solarized.
-# i.e. if "blue" is #268bd2, not whatever the default is.
+# Use these settings if you've applied a Solarized theme to your terminal (for
+# example, if "ls -G" produces Solarized output). i.e. if "blue" is #268bd2, not
+# whatever the default is. (See "../etc/Solarized Dark.terminal" for OS X.)
 
 set -l base03  "--bold black"
 set -l base02  "black"
@@ -40,6 +20,27 @@ set -l violet  "--bold magenta"
 set -l blue    "blue"
 set -l cyan    "cyan"
 set -l green   "green"
+
+# Use these settings if your terminal supports term256 and your terminal hasn't
+# been configured with a Solarized theme.i.e. if "blue" is the default blue, not
+# Solarized blue.
+#
+# set -l base03  002b36
+# set -l base02  073642
+# set -l base01  586e75
+# set -l base00  657b83
+# set -l base0   839496
+# set -l base1   93a1a1
+# set -l base2   eee8d5
+# set -l base3   fdf6e3
+# set -l yellow  b58900
+# set -l orange  cb4b16
+# set -l red     dc322f
+# set -l magenta d33682
+# set -l violet  6c71c4
+# set -l blue    268bd2
+# set -l cyan    2aa198
+# set -l green   859900
 
 # Used by fish's completion; see
 # http://fishshell.com/docs/2.0/index.html#variables-color
