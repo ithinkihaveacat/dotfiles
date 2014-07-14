20d15a162a7cce6058d2a8ea0e7e3238c1a745f4
Michael Stillwell <mjs@beebo.org>
2014-07-14 09:22:58 +0100

    Support defaults

--- fish/functions/randombase64.fish
+++ fish/functions/randombase64.fish
@@ -1,4 +1,5 @@
 function randombase64 -d "Generate random base64 string"
+  set -l n $argv[1]; test -z $n; and set n 12
   # https://en.wikipedia.org/wiki/Birthday_problem#Probability_table
-  dd if=/dev/urandom bs=1 count=128 ^/dev/null | base64 | cut -b -12
+  dd if=/dev/urandom bs=1 count=128 ^/dev/null | base64 | cut -b -$n
 end
--- fish/functions/randomhex.fish
+++ fish/functions/randomhex.fish
@@ -1,4 +1,5 @@
 function randomhex -d "Generate random hexadecimal string"
+  set -l n $argv[1]; test -z $n; and set n 12
   # https://en.wikipedia.org/wiki/Birthday_problem#Probability_table
-  dd if=/dev/urandom bs=1 count=128 ^/dev/null | gmd5sum - | cut -b -12
+  dd if=/dev/urandom bs=1 count=1024 ^/dev/null | gmd5sum - | cut -b -$n
 end
