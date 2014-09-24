c262efb89e64bf8cf126d00c080396d52215856f
Michael Stillwell <mjs@beebo.org>
2014-09-24 20:27:13 +0100

    Add gem bin directory to path

--- fish/config.fish
+++ fish/config.fish
@@ -40,6 +40,11 @@ if test -d ~/local/google-cloud-sdk/bin
   set fish_user_paths $fish_user_paths ~/local/google-cloud-sdk/bin
 end
 
+# gem install $name --user-install
+if test -d ~/.gem/ruby/*/bin
+  set fish_user_paths $fish_user_paths ~/.gem/ruby/*/bin
+end
+
 # http://fishshell.com/docs/2.1/#variables-special
 set --erase fish_greeting
 
