4aaa03cb267d911daf844d16fa13814ae939c033
Michael Stillwell <mjs@beebo.org>
2014-09-24 16:27:03 +0100

    Generate icons

---
+++ fish/functions/gam-icons.fish
@@ -0,0 +1,13 @@
+function gam-icons -d "Generate icons in different sizes"
+  if test ( count $argv ) -ne 1
+    echo "usage: $_ color"
+    return
+  end
+  convert -size 16x16 xc:$argv[1] 16x16.png
+  convert -size 32x32 xc:$argv[1] 32x32.png
+  convert -size 48x48 xc:$argv[1] 48x48.png
+  convert -size 96x96 xc:$argv[1] 96x96.png
+  convert -size 128x128 xc:$argv[1] 128x128.png
+  convert -size 440x280 xc:$argv[1] 440x280.png
+  convert -size 1280x800 xc:$argv[1] 1280x800.png
+end
