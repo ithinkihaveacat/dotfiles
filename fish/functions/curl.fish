45155f8333dd12a842f52c44023b58bcc4e01638
Michael Stillwell <mjs@beebo.org>
2014-11-04 09:55:36 +0000

    Remove curl function, was broken somehow

--- fish/functions/curl.fish
+++
@@ -1,8 +0,0 @@
-function curl -d "transfer a URL, with Authorization support"
-  if test -n "$CURL_AUTHORIZATION"
-    set header "-H \"Authorization: $CURL_AUTHORIZATION\""
-  else
-    set header ""
-  end
-  command curl $header $argv
-end
