5073955b6435af82c1bc87aaa376dc5f795bc590
Michael Stillwell <mjs@beebo.org>
2015-04-20 00:21:55 +0100

    Add e function

---
+++ fish/functions/e.fish
@@ -0,0 +1,18 @@
+function e -d "Search for filename in $PATH, edit with $EDITOR"
+  if test ( count $argv ) -ne 1
+    echo "usage: $_ filename"
+    return
+  end
+  if test -f $argv[1]
+    eval $EDITOR $argv[1]
+  end
+  if type -P $argv[1] >/dev/null
+    if file (type -P $argv[1]) | grep text >/dev/null
+      eval $EDITOR (type -P $argv[1])
+    else
+      echo "$_: '$argv[1]' is not a text file"
+    end
+  else
+    echo "$_: Could not find '$argv[1]'"
+  end
+end
