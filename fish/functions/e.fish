749356583e568b5f1124464182df2c2c15b98695
Michael Stillwell <mjs@beebo.org>
2015-05-11 16:20:44 +0100

    Add comment

--- fish/functions/e.fish
+++ fish/functions/e.fish
@@ -6,6 +6,7 @@ function e -d 'Search for filename in $PATH, edit with $EDITOR'
   if test -f $argv[1]
     eval $EDITOR $argv[1]
   end
+  # Might be able to switch to "command" in fish 2.2+
   if type -P $argv[1] >/dev/null
     if file (type -P $argv[1]) | grep text >/dev/null
       eval $EDITOR (type -P $argv[1])
