83b521aae3d604e2dadd794089c219c6fd9d2ead
Michael Stillwell <mjs@beebo.org>
2015-08-20 07:48:24 +0100

    Change to hello-world-web

--- fish/functions/make.hello-world-web.fish
+++ fish/functions/make.hello-world-web.fish
@@ -3,5 +3,5 @@ function make.hello-world-web
     echo "usage: $_ directory"
     return
   end
-  svn export https://github.com/ithinkihaveacat/Skeleton/branches/roboto $argv[1]
+  svn export https://github.com/ithinkihaveacat/hello-world-web/branches/master $argv[1]
 end
