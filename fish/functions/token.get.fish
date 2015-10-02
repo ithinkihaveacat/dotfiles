4d30247bd0ea75c42a9c5cc505162c8f3826433c
Michael Stillwell <mjs@beebo.org>
2015-10-02 13:57:08 +0100

    Use AUTH_URI and TOKEN_URI

--- fish/functions/token.get.fish
+++ fish/functions/token.get.fish
@@ -18,15 +18,19 @@ function token.get -d "Gets an access token via OpenID Connect"
     return
   end
 
+  # https://accounts.google.com/.well-known/openid-configuration
+  set -l AUTH_URI "https://accounts.google.com/o/oauth2/v2/auth"
+  set -l TOKEN_URI "https://www.googleapis.com/oauth2/v4/token"
+
   set -l REDIRECT_URI "urn:ietf:wg:oauth:2.0:oob"
   set -l SCOPE (echo "openid $argv" | perl -MURI::Escape -ne 'chomp; print uri_escape($_);')
 
   # https://developers.google.com/identity/protocols/OpenIDConnect?hl=en#authenticationuriparameters
-  echo "https://accounts.google.com/o/oauth2/v2/auth?client_id=$CLIENT_ID&response_type=code&scope=$SCOPE&redirect_uri=$REDIRECT_URI" | pbcopy
+  echo "$AUTH_URI?client_id=$CLIENT_ID&response_type=code&scope=$SCOPE&redirect_uri=$REDIRECT_URI" | pbcopy
 
   echo "OpenID Connect URL copied to your clipboard; load it, and paste the returned code below"
   read -l CODE
 
-  curl https://www.googleapis.com/oauth2/v4/token -d client_id=$CLIENT_ID -d client_secret=$CLIENT_SECRET -d redirect_uri=$REDIRECT_URI -d grant_type=authorization_code -d code=$CODE
+  curl $TOKEN_URI -d client_id=$CLIENT_ID -d client_secret=$CLIENT_SECRET -d redirect_uri=$REDIRECT_URI -d grant_type=authorization_code -d code=$CODE
 
 end
