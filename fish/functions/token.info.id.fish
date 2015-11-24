02c26595cedf94b49be5a9496b9962aafe80548e
Michael Stillwell <mjs@beebo.org>
2015-11-24 15:25:38 +0000

    Update docs

--- fish/functions/token.info.id.fish
+++ fish/functions/token.info.id.fish
@@ -1,6 +1,6 @@
-function token.info.id -d "Get information about a Google access token"
+function token.info.id -d "Get information about a Google id token"
   if test ( count $argv ) -eq 0
-    echo "usage: $_ access_token"
+    echo "usage: $_ id_token"
     return
   end
   set url "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=$argv[1]"
