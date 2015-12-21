bb90800a5d34034604bb37335b6e036102f8d4ea
Michael Stillwell <mjs@beebo.org>
2015-12-21 17:29:30 +0000

    Cleanup token.refresh

--- fish/functions/token.refresh.fish
+++ fish/functions/token.refresh.fish
@@ -1,10 +1,27 @@
 # https://developers.google.com/accounts/docs/OAuth2WebServer#refresh
 
 function token.refresh -d "Get access token from a refresh token"
-  if test ( count $argv ) -ne 3
-    echo "usage: $_ client_id client_secret refresh_token"
+
+  # set -xU CLIENT_ID "212696220856-2mrcaejro9o098rpdod53kpld0athcuf.apps.googleusercontent.com"
+  # set -xU CLIENT_SECRET "xK0v4Ldw_8BUWnrTQJn3YbEG"
+
+  if test -z "$CLIENT_ID"
+    echo "error: CLIENT_ID not set; create 'Other' client via https://console.developers.google.com/project"
     return
   end
-  set url "https://accounts.google.com/o/oauth2/token"
-  curl -s $url -d client_id=$argv[1] -d client_secret=$argv[2] -d refresh_token=$argv[3] -d grant_type=refresh_token
+
+  if test -z "$CLIENT_SECRET"
+    echo "error: CLIENT_SECRET not set; create 'Other' client via https://console.developers.google.com/project"
+    return
+  end
+
+  if test ( count $argv ) -ne 1
+    echo "usage: $_ refresh_token"
+    return
+  end
+
+  set URI "https://accounts.google.com/o/oauth2/token"
+
+  curl -s $URI -d client_id=$CLIENT_ID -d client_secret=$CLIENT_SECRET -d refresh_token=$argv[1] -d grant_type=refresh_token
+
 end
