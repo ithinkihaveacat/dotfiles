174880301dfbe7095f9e3d4f7a34f8a20ea33b63
Michael Stillwell <mjs@beebo.org>
2016-02-05 10:21:05 +0000

    Make e work better

--- fish/functions/e.fish
+++ fish/functions/e.fish
@@ -1,19 +1,37 @@
-function e -d 'Search for filename in $PATH, edit with $EDITOR'
+function e -d 'Edit file, searching in a few different places'
+
   if test ( count $argv ) -ne 1
     echo "usage: $_ filename"
     return
   end
-  if test -f $argv[1]
-    eval $EDITOR $argv[1]
+
+  if not type -qt $argv[1]
+    echo "$_: '$argv[1]' not found"
+    return
   end
-  # Might be able to switch to "command" in fish 2.2+ (or type -q)
-  if type -P $argv[1] >/dev/null
-    if file (type -P $argv[1]) | grep text >/dev/null
-      eval $EDITOR (type -P $argv[1])
-    else
-      echo "$_: '$argv[1]' is not a text file"
-    end
-  else
-    echo "$_: Could not find '$argv[1]'"
+
+  switch (type -t $argv[1])
+
+    case file
+
+      if file (type -p $argv[1]) | grep text >/dev/null
+        eval $EDITOR (type -p $argv[1])
+      else
+        echo "error: '$argv[1]' is not a text file"
+      end
+
+    case function
+
+      if test -f $HOME/.config/fish/functions/$argv[1].fish
+        eval $EDITOR $HOME/.config/fish/functions/$argv[1].fish
+      else
+        echo "error: '$argv[1]' not defined in $HOME/.config/fish/functions"
+      end
+
+    case '*'
+
+      echo "error: '$argv[1]' not found"
+
   end
+
 end
