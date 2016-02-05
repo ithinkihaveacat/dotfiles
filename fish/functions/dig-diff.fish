a764d1b26cd4963935b2c97d79210980c07e098b
Michael Stillwell <mjs@beebo.org>
2016-02-05 09:59:05 +0000

    Remove ttl field from dig output

--- fish/functions/dig-diff.fish
+++ fish/functions/dig-diff.fish
@@ -8,8 +8,8 @@ function dig-diff -d "Compares cached and uncached DNS records"
   set -l NS (dig +short $argv[1] ns | head -1 | rev | cut -c 2- | rev)
 
   set -l ARGS --minimal --new-line-format='+%L' --old-line-format='-%L' --unchanged-line-format='%L'
-  set -l DIG1 dig -t ANY +noall +answer $argv[1] @$NS
-  set -l DIG2 dig -t ANY +noall +answer $argv[1]
+  set -l DIG1 dig -t ANY +noall +nottl +answer $argv[1]
+  set -l DIG2 dig -t ANY +noall +nottl +answer $argv[1] @$NS
   
   echo "# $DIG1"
   echo "# $DIG2"
