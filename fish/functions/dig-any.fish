775331978eef6f192691a916e0209582ce6825ac
Michael Stillwell <mjs@beebo.org>
2016-02-06 13:12:46 +0000

    Switch from ANY to possible types

---
+++ fish/functions/dig-any.fish
@@ -0,0 +1,16 @@
+function dig-any -d "DNS lookup (all records)"
+
+  # DNS queries for the "ANY" type behave unpredictably, partly
+  # because of caches, and partly because authoritative servers don't
+  # like it. Simulate an "ANY" query by asking for all "likely" types.
+  # http://serverfault.com/q/754422/14573
+  
+  set -l QUERY
+  # https://en.wikipedia.org/wiki/List_of_DNS_record_types
+  for type in A AAAA NS SOA MX TXT NAPTR CNAME DNSKEY RRSIG RP SRV
+    set QUERY $QUERY $argv[1] $type
+  end
+
+  dig +noall +nottl +answer $QUERY | sort | uniq
+
+end
--- fish/functions/dig-diff.fish
+++ fish/functions/dig-diff.fish
@@ -7,13 +7,23 @@ function dig-diff -d "Compares cached and uncached DNS records"
 
   set -l NS (dig +short $argv[1] NS | sort | head -1 | rev | cut -c 2- | rev)
 
+  # DNS queries for the "ANY" type behave unpredictably, partly
+  # because of caches, and partly because authoritative servers don't
+  # like it. Simulate an "ANY" query by asking for all "likely" types.
+  # http://serverfault.com/q/754422/14573
+  
+  set -l QUERY
+  for type in A AAAA NS SOA MX TXT NAPTR CNAME DNSKEY RRSIG RP SRV
+    set QUERY $QUERY $argv[1] $type
+  end
+  
   set -l ARGS --minimal --new-line-format='+%L' --old-line-format='-%L' --unchanged-line-format='%L'
-  set -l DIG1 dig -t ANY +noall +nottl +answer $argv[1]
-  set -l DIG2 dig -t ANY +noall +nottl +answer $argv[1] @$NS
+  set -l DIG1 dig +noall +nottl +answer $QUERY
+  set -l DIG2 dig +noall +nottl +answer $QUERY @$NS
   
   echo "# $DIG1"
   echo "# $DIG2"
 
-  diff $ARGS (eval $DIG1 | sort | psub) (eval $DIG2 | sort | psub)
+  diff $ARGS (eval $DIG1 | sort | uniq | psub) (eval $DIG2 | sort | uniq | psub)
 
 end
