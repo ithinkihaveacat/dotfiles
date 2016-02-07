ffdab82c3eaab6007a520b09ee82745b00dfa2be
Michael Stillwell <mjs@beebo.org>
2016-02-07 19:33:22 +0000

    Remove RRSIG, can be slow

--- fish/functions/dig-diff.fish
+++ fish/functions/dig-diff.fish
@@ -13,7 +13,7 @@ function dig-diff -d "Compares cached and uncached DNS records"
   # http://serverfault.com/q/754422/14573
   
   set -l QUERY
-  for type in A AAAA NS SOA MX TXT NAPTR CNAME DNSKEY RRSIG RP SRV
+  for type in A AAAA NS SOA MX TXT NAPTR CNAME DNSKEY RP SRV
     set QUERY $QUERY $argv[1] $type
   end
   
