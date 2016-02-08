8e03fc175d746754ee2e3326675902040527eb55
Michael Stillwell <mjs@beebo.org>
2016-02-08 20:23:32 +0000

    Convert to shell script

--- fish/functions/make.zip-archive.fish
+++
@@ -1,7 +0,0 @@
-function make.zip-archive -d "Creates zip file from current git repo's HEAD"
-  if test ( count $argv ) -ne 1
-    echo "usage: $_ zipfile"
-    return
-  end
-  git archive --format=zip --prefix= -o $argv[1] HEAD
-end
