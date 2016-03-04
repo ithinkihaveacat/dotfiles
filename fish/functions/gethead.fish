fee674304d99ddbfb351b9ce97ceba83bad47bce
Michael Stillwell <mjs@beebo.org>
2016-03-04 08:39:37 +0000

    Avoid pipe (and perl)

--- fish/functions/gethead.fish
+++ fish/functions/gethead.fish
@@ -1,3 +1,4 @@
-function gethead -d "Retrieve single URL, display headers only"
-  curl -s -i --max-redirs 0 $argv | perl -ne 'print if 1 .. /^\s*$/'
+function gethead -d "Retrieve single URL, displaying headers only"
+  curl -s -D - -o /dev/null $argv
+  # curl -s -i --max-redirs 0 $argv | perl -ne 'print if 1 .. /^\s*$/'
 end
