44b09eb3b498ff86b6af2ee3f00c025ca51b0020
Michael Stillwell <mjs@beebo.org>
2016-05-06 10:38:20 +0100

    Provide auth header if $ACCESS_TOKEN defined

--- fish/functions/getraw.fish
+++ fish/functions/getraw.fish
@@ -1,3 +1,7 @@
 function getraw -d "Retrieve single URL (including headers), output to stdout"
-  curl -s -i --max-redirs 0 $argv
+  if test -n "$ACCESS_TOKEN"
+    curl -s -i --max-redirs 0 -H "Authorization: Bearer $ACCESS_TOKEN" $argv
+  else
+    curl -s -i --max-redirs 0 $argv
+  end
 end
