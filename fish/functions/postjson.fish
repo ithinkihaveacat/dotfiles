bd15f996fad570d3e4487a32086a3bd1b6158c6d
Michael Stillwell <mjs@beebo.org>
2016-08-17 13:56:04 +0100

    Update postjson

--- fish/functions/postjson.fish
+++ fish/functions/postjson.fish
@@ -1,4 +1,6 @@
 function postjson -d "POSTs JSON from stdin to URL"
-  # echo '{"bar": 2}' | postjson http://localhost:8080/api/whoami
-  curl -s --max-redirs 0 -X POST --data @- -H 'content-type: application/json' $argv
+  if test -n "$ACCESS_TOKEN"
+    set -l AUTHORIZATION -H "authorization: Bearer $ACCESS_TOKEN"
+  end
+  curl -s --max-redirs 0 -X POST --data @- $AUTHORIZATION -H 'content-type: application/json' $argv
 end
