e8405068b541a9aada0fa880136ed45994a39f61
Michael Stillwell <mjs@beebo.org>
2016-09-21 12:23:34 +0100

    Make STRATEGY configurable

--- fish/functions/ampurls.fish
+++ fish/functions/ampurls.fish
@@ -7,6 +7,9 @@ function ampurls -d "Lookup URL in AMP Cache"
     echo "usage: $_ url"
     return 1
   end
+  # https://developers.google.com/amp/cache/reference/acceleratedmobilepageurl/rest/v1/ampUrls/batchGet#lookupstrategy
+  set STRATEGY IN_INDEX_DOC
+  # set STRATEGY FETCH_LIVE_DOC
   # https://developers.google.com/amp/cache/reference/acceleratedmobilepageurl/rest/v1/ampUrls/batchGet
-  curl -s -X POST -d "{urls:['"$argv[1]"']}" -H "x-goog-api-key: $AMP_API_KEY" -H 'content-type: application/json' https://acceleratedmobilepageurl.googleapis.com/v1/ampUrls:batchGet
+  curl -s -X POST -d "{urls:['"$argv[1]"'],lookupStrategy:'"$STRATEGY"'}" -H "x-goog-api-key: $AMP_API_KEY" -H 'content-type: application/json' https://acceleratedmobilepageurl.googleapis.com/v1/ampUrls:batchGet
 end
