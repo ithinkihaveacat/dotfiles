6d29f1b8e01534323935796c800949c76677ef7a
Michael Stillwell <mjs@beebo.org>
2016-11-02 13:10:04 +0000

    Add --group-directories-first

---
+++ fish/functions/ls.fish
@@ -0,0 +1,35 @@
+# Copied from /usr/share/fish/functions/ls.fish
+
+if command ls --version 1>/dev/null 2>/dev/null
+	# This is GNU ls
+	function ls --description "List contents of directory"
+		set -l param --color=auto --group-directories-first
+		if isatty 1
+			set param $param --indicator-style=classify
+		end
+		command ls $param $argv
+	end
+
+	if not set -q LS_COLORS
+		if type -q -f dircolors
+			set -l colorfile
+			for file in ~/.dir_colors ~/.dircolors /etc/DIR_COLORS
+				if test -f $file
+					set colorfile $file
+					break
+				end
+			end
+			echo $colorfile
+			eval (dircolors -c $colorfile | sed 's/>&\/dev\/null$//')
+		end
+	end
+
+else
+	# BSD, OS X and a few more support colors through the -G switch instead
+	if command ls -G / 1>/dev/null 2>/dev/null
+		function ls --description "List contents of directory"
+			command ls -G $argv
+		end
+	end
+end
+
