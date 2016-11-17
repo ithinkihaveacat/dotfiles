ab6ebdc22bec90b1ce219e98c59fa72f3a34717e
Michael Stillwell <mjs@beebo.org>
2016-11-17 06:28:20 +0000

    Add font-go

---
+++ fish/functions/font-go.fish
@@ -0,0 +1,16 @@
+# https://github.com/golang/image
+
+function font-go
+
+  set DIR (fontdir)/go
+  set URL 'https://api.github.com/repos/golang/image/zipball'
+
+  if test -d $DIR
+    echo "error: $DIR already exists"
+    return 1
+  end
+
+  mkdir -p $DIR
+  unzip -j (curl -sL $URL | psub) '*.ttf' -d $DIR
+
+end
