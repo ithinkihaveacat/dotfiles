22fa0e23bdbc411f1edf036d2620fca8d6f7e552
Michael Stillwell <mjs@beebo.org>
2016-12-19 13:38:03 +0000

    Add UA strings

--- fish/ua.fish
+++ fish/ua.fish
@@ -3,3 +3,8 @@
 # https://support.google.com/webmasters/answer/1061943?hl=en
 set -xg UA_GOOGLEBOT 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 set -xg UA_GOOGLEBOT_MOBILE 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+
+# From Chrome Devtools
+set -xg UA_CHROME_MOBILE 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.28 Mobile Safari/537.36'
+set -xg UA_CHROME_DESKTOP 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.28 Safari/537.36'
+set -xg UA_SAFARI_MOBILE 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
