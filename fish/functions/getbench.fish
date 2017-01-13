20ac1160f08a6f09f9dbc008613d49721be1ceb2
Michael Stillwell <mjs@beebo.org>
2017-01-13 17:21:54 +0000

    Improve getbench

--- fish/functions/getbench.fish
+++ fish/functions/getbench.fish
@@ -1,3 +1,3 @@
 function getbench -d "How long does it take to retrieve a URL?"
-  curl -kso /dev/null -w "dns: %{time_namelookup} tcp:%{time_connect} ssl:%{time_appconnect} ttfb:%{time_starttransfer}\n" $argv
+  curl -kso /dev/null -w "dns:%{time_namelookup} tcp:%{time_connect} ssl:%{time_appconnect} ttfb:%{time_starttransfer} total:%{time_total} (%{size_download} bytes)\n" $argv
 end
