071d231b5d92cb424565bf882072b06d2a556090
Michael Stillwell <mjs@beebo.org>
2017-03-22 16:00:14 +0000

    Improve amp-google-*

--- fish/functions/amp-google-status-live.fish
+++ fish/functions/amp-google-status-live.fish
@@ -1,10 +1,10 @@
-function amp-google-validator -d "Query Google AMP CDN for URL validity (FETCH_LIVE_DOC strategy)"
+function amp-google-status-live -d "Query Google AMP CDN for URL validity (FETCH_LIVE_DOC strategy)"
   if test -z "$AMP_API_KEY"
     # https://developers.google.com/amp/cache/reference/authorizing
     echo "error: AMP_API_KEY must be set"
   end
   if test ( count $argv ) -eq 0
-    echo "usage: $_ url"
+    echo "usage: $_ canonical_url"
     return 1
   end
   # https://developers.google.com/amp/cache/reference/acceleratedmobilepageurl/rest/v1/ampUrls/batchGet#lookupstrategy
