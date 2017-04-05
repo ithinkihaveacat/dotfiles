09f562948a030133d48ba7dbedcca5070accebed
Michael Stillwell <mjs@beebo.org>
2017-04-05 01:18:55 +0100

    Add font-vollkorn

---
+++ fish/functions/font-vollkorn.fish
@@ -0,0 +1,16 @@
+# http://vollkorn-typeface.com/
+
+function font-vollkorn
+
+  set DIR (fontdir)/vollkorn
+  set URL 'http://vollkorn-typeface.com/download/vollkorn-4-012.zip'
+
+  if test -d $DIR
+    echo "error: $DIR already exists"
+    return 1
+  end
+
+  mkdir -p $DIR
+  unzip -j (curl -sL $URL | psub) '*.ttf' -d $DIR
+
+end
