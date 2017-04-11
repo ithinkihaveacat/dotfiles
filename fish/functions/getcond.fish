8489c5d123ea8d62ac5fab63e20792ed633413b4
Michael Stillwell <mjs@beebo.org>
2017-04-11 12:33:25 +0100

    Detect 404s

--- fish/functions/getcond.fish
+++ fish/functions/getcond.fish
@@ -1,4 +1,4 @@
-function getcond -d "Retrieve URL, then retrieve conditionally"
+function getcond -d "Conditionally retrieve URL"
 
   if test (count $argv) -ne 1
     echo "error: $_ URL"
@@ -11,7 +11,13 @@ function getcond -d "Retrieve URL, then retrieve conditionally"
     return 1
   end
 
-  set -l etag (curl -s -D - -o /dev/null $url | grep -Ei '^etag' | awk '{ printf "%s", $2 }')
+  set -l httpcode (curl -s -o /dev/null -w "%{http_code}" $url)
+  if test $httpcode -ne 200
+    curl -s -D - -o /dev/null $url
+    return 1
+  end
+
+  set -l etag (curl -s -D - -o /dev/null $url | grep -Ei '^etag' | awk 'BEGIN { FS = ": " } { print $2 }')
 
   if test -n (echo $etag)
     set -l header (printf "if-none-match: %s" (string trim $etag))
@@ -24,7 +30,8 @@ function getcond -d "Retrieve URL, then retrieve conditionally"
     echo
   end
 
-  set -l lastmodified (curl -s -D - -o /dev/null $url | grep -Ei '^last-modified' | awk '{ $1 = ""; printf "%s", $0 }')
+  set -l lastmodified (curl -s -D - -o /dev/null $url | grep -Ei '^last-modified' | awk 'BEGIN { FS = ": " } { print $2 }')
+
   if test -n (echo $lastmodified)
     set -l header (printf "if-modified-since: %s" (string trim $lastmodified))
     echo "# last-modified"
@@ -34,8 +41,5 @@ function getcond -d "Retrieve URL, then retrieve conditionally"
     echo
     curl -s -D - -o /dev/null -H $header $url | head -1
   end
-
-#  set -l httpcode (curl -s -o /dev/null -H $header -w "%{http_code}" $url)
-#  echo $httpcode
 
 end
