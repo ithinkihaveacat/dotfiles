afd54bffecaaa85811a4ef73c26bda0e2f94c775
Michael Stillwell <mjs@beebo.org>
2017-05-01 16:54:08 +0100

    Attempt to missing filename a bit better

--- fish/functions/geturl.fish
+++ fish/functions/geturl.fish
@@ -1,7 +1,10 @@
 function geturl -d "Save URL to file"
   if test -n "$ACCESS_TOKEN"
-    curl -s -H "Authorization: Bearer $ACCESS_TOKEN" -O $argv
+    curl -s -H "Authorization: Bearer $ACCESS_TOKEN" --remote-name-all $argv
+    if test "$status" -eq 23
+      echo "error: $_ "
+    end
   else
-    curl -s -O $argv
+    curl -s --remote-name-all $argv
   end
 end
