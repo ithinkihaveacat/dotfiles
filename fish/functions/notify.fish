af0d3b2cf97f09485d2403d7bf3efd3a214e9c3e
Michael Stillwell <mjs@beebo.org>
2017-06-23 12:10:22 +0100

    Make notify work on Ubuntu

--- fish/functions/notify.fish
+++ fish/functions/notify.fish
@@ -1,10 +1,24 @@
-function notify -a body -a title -d "Posts a notification using the Notification Center"
-  # https://developer.apple.com/library/mac/documentation/applescript/conceptual/applescriptlangguide/reference/aslr_cmds.html#//apple_ref/doc/uid/TP40000983-CH216-SW224
+function notify -a body -a title -d "Posts a notification using the native notification system"
+
   if [ -z "$body" ]
     set body done
   end
   if [ -z "$title" ]
     set title Terminal
   end
-  osascript -e "display notification \"$body\" with title \"$title\""
+
+  if type -q osascript
+    # https://developer.apple.com/library/mac/documentation/applescript/conceptual/applescriptlangguide/reference/aslr_cmds.html#//apple_ref/doc/uid/TP40000983-CH216-SW224
+    osascript -e "display notification \"$body\" with title \"$title\""
+    return
+  end
+
+  if type -q notify-send
+    notify-send $title $body
+    return
+  end
+
+  echo "error: unsupported system"
+  return 1
+
 end
