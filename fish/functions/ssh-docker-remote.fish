01fb76ca5deb602ef432982b82c5ffe2fe8706f4
Michael Stillwell <mjs@beebo.org>
2017-08-09 00:23:11 +0100

    Add ssh-docker-*

---
+++ fish/functions/ssh-docker-remote.fish
@@ -0,0 +1,5 @@
+# When configured via docker/Dockerfile
+function ssh-docker-remote
+  # TODO Copy ssh key to ~/iCloud
+  ssh -i (docker-machine inspect -f "{{.HostOptions.AuthOptions.StorePath}}")/id_rsa -p 80 mjs@(docker-machine ip)
+end
