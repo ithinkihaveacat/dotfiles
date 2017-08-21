5d8733043ff661160726352c2384ea389c6779bc
Michael Stillwell <mjs@beebo.org>
2017-08-21 14:05:45 +0100

    Handle non Ubuntu/macOS

--- fish/functions/terminal-frontmost.fish
+++ fish/functions/terminal-frontmost.fish
@@ -1,12 +1,19 @@
 if test -n "$WINDOWID"
+  # X Windows
   function terminal-frontmost
     test "$WINDOWID" = (windowid-frontmost)
   end function
-else
+else if test -n "$TERM_SESSION_ID"
+  # macOS
   function terminal-frontmost
     # tmp so that an empty tty-frontmost results in an empty string; see
     # https://github.com/fish-shell/fish-shell/issues/159
     set tmp (tty-frontmost)
     test (tty) = "$tmp"
+  end function
+else
+  # Not a graphical environment, or unknown
+  function terminal-frontmost
+    return 0
   end function
 end if
