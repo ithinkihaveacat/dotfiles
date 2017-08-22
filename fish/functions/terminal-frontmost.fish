c17bf36f15510eb8b3ad0cb5fd4f2347ca1a4fa5
Michael Stillwell <mjs@beebo.org>
2017-08-22 08:22:08 +0100

    Inline terminal-frontmost

--- fish/functions/terminal-frontmost.fish
+++ fish/functions/terminal-frontmost.fish
@@ -1,19 +1,35 @@
 if test -n "$WINDOWID"
+
   # X Windows
   function terminal-frontmost
-    test "$WINDOWID" = (windowid-frontmost)
+    set -l tmp (printf "%d" (string -m 1 -r (xprop -root _NET_ACTIVE_WINDOW))[2])
+    test "$WINDOWID" = "$tmp"
   end function
+
 else if test -n "$TERM_SESSION_ID"
+
   # macOS
   function terminal-frontmost
-    # tmp so that an empty tty-frontmost results in an empty string; see
-    # https://github.com/fish-shell/fish-shell/issues/159
-    set tmp (tty-frontmost)
+    set -l tmp (osascript \
+      -e 'tell application "Terminal"' \
+      -e '  if frontmost is true' \
+      -e '    repeat with w in windows' \
+      -e '      if (frontmost of w) is true then' \
+      -e '        set t to (selected tab of w)' \
+      -e '        return (tty of t) as string' \
+      -e '      end if' \
+      -e '    end repeat' \
+      -e '  end if' \
+      -e 'end tell' \
+    )
     test (tty) = "$tmp"
   end function
+
 else
-  # Not a graphical environment, or unknown
+
+  # Unknown/other
   function terminal-frontmost
-    return 0
+    return 0 # Always frontmost
   end function
+
 end if
