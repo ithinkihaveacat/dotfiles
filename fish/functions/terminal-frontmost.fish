77330bfcf7d2283189cd0dcee935d681498925d1
Michael Stillwell <mjs@beebo.org>
2017-08-22 16:13:22 +0100

    Add description

--- fish/functions/terminal-frontmost.fish
+++ fish/functions/terminal-frontmost.fish
@@ -1,7 +1,7 @@
 if test -n "$WINDOWID"
 
   # X Windows
-  function terminal-frontmost
+  function terminal-frontmost -d "Returns 0 if terminal is frontmost, otherwise 1"
     set -l tmp (printf "%d" (string split -m 1 -r " " (xprop -root _NET_ACTIVE_WINDOW))[2])
     test "$WINDOWID" = "$tmp"
   end function
@@ -9,7 +9,7 @@ if test -n "$WINDOWID"
 else if test -n "$TERM_SESSION_ID"
 
   # macOS
-  function terminal-frontmost
+  function terminal-frontmost -d "Returns 0 if terminal is frontmost, otherwise 1"
     set -l tmp (osascript \
       -e 'tell application "Terminal"' \
       -e '  if frontmost is true' \
@@ -28,7 +28,7 @@ else if test -n "$TERM_SESSION_ID"
 else
 
   # Unknown/other
-  function terminal-frontmost
+  function terminal-frontmost -d "Returns 0 if terminal is frontmost, otherwise 1"
     return 0 # Always frontmost
   end function
 
