9d513ed7cdb2bf7f3d32e669b17dd56134d2f0d3
Michael Stillwell <mjs@beebo.org>
2017-09-05 22:59:15 +0100

    Improve docs

--- fish/functions/terminal-frontmost.fish
+++ fish/functions/terminal-frontmost.fish
@@ -1,6 +1,9 @@
+# If on X, but no WINDOWID, investigate `xdotool getwindowfocus`.
+# http://manpages.ubuntu.com/manpages/trusty/man1/xdotool.1.html
+# https://pleasantprogrammer.com/posts/is-my-terminal-window-active.html
 if test -n "$WINDOWID"
 
-  # X Windows
+  # X Windows (supports Ubuntu Terminal excluding tabs)
   function terminal-frontmost -d "Returns 0 if terminal is frontmost, otherwise 1"
     set -l tmp (printf "%d" (string split -m 1 -r " " (xprop -root _NET_ACTIVE_WINDOW))[2])
     test "$WINDOWID" = "$tmp"
@@ -8,7 +11,7 @@ if test -n "$WINDOWID"
 
 else if test -n "$TERM_SESSION_ID"
 
-  # macOS
+  # macOS (supports Terminal.app and tabs)
   function terminal-frontmost -d "Returns 0 if terminal is frontmost, otherwise 1"
     set -l tmp (osascript \
       -e 'tell application "Terminal"' \
