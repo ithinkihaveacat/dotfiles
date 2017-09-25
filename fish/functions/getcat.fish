4630cc7e960ec9c59af4beb08eb0d511d3811530
Michael Stillwell <mjs@beebo.org>
2017-09-25 12:48:31 +0100

    Follow links, always output to stdout

--- fish/functions/getcat.fish
+++ fish/functions/getcat.fish
@@ -1,10 +1,10 @@
 function getcat -d "Retrieve single URL, output to stdout"
   if test -n "$ACCESS_TOKEN"
-    curl -sS -H "Authorization: Bearer $ACCESS_TOKEN" $argv
+    curl -sSL --output - -H "Authorization: Bearer $ACCESS_TOKEN" $argv
     if test "$status" -eq 23
       echo "error: $_ "
     end
   else
-    curl -sS $argv
+    curl -sSL --output - $argv
   end
 end
