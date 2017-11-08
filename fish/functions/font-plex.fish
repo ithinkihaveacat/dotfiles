1d80f3af6d59669f527a3276d0c7b2c86879e21b
Michael Stillwell <mjs@beebo.org>
2017-11-08 15:20:32 +0000

    Add font-plex

---
+++ fish/functions/font-plex.fish
@@ -0,0 +1,16 @@
+# https://ibm.github.io/type/
+
+function font-plex
+
+  set DIR (fontdir)/plex
+  set URL 'https://github.com/IBM/type/archive/master.zip'
+
+  if test -d $DIR
+    echo "error: $DIR already exists"
+    return 1
+  end
+
+  mkdir -p $DIR
+  unzip -j (curl -sL $URL | psub) 'type-master/fonts/*/*.otf' -d $DIR
+
+end
