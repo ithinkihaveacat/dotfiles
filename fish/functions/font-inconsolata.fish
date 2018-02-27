96ef15792eb78992ddb9c97cded2edd820a21a78
Michael Stillwell <mjs@beebo.org>
2018-02-27 10:37:16 +0000

    Add font-inconsolata

---
+++ fish/functions/font-inconsolata.fish
@@ -0,0 +1,17 @@
+# http://levien.com/type/myfonts/inconsolata.html
+
+function font-inconsolata
+
+  set DIR (fontdir)/inconsolata
+  set URL 'https://raw.githubusercontent.com/google/fonts/master/ofl/inconsolata/Inconsolata-%s.ttf'
+
+  if test -d $DIR
+    echo "error: $DIR already exists"
+    return 1
+  end
+
+  mkdir -p $DIR
+  curl -sS (printf $URL Bold) > $DIR/Inconsolata-Bold.ttf
+  curl -sS (printf $URL Regular) > $DIR/Inconsolata-Regular.ttf
+
+end
