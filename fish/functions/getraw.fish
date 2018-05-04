48895daca33e79c139d544984b8d13b6c201e305
Michael Stillwell <mjs@beebo.org>
2018-05-04 16:22:33 +0100

    Always output to stdout

--- fish/functions/getraw.fish
+++ fish/functions/getraw.fish
@@ -1,7 +1,7 @@
 function getraw -w curl -d "Retrieve single URL (including headers), output to stdout"
   if test -n "$ACCESS_TOKEN"
-    curl -sS -i --max-redirs 0 -H "Authorization: Bearer $ACCESS_TOKEN" $argv
+    curl -sS -i --max-redirs 0 --ouptut - -H "Authorization: Bearer $ACCESS_TOKEN" $argv
   else
-    curl -sS -i --max-redirs 0 $argv
+    curl -sS -i --max-redirs 0 --output - $argv
   end
 end
