0d4f53759778666723140efe6d32a13993649e59
Michael Stillwell <mjs@beebo.org>
2018-06-04 16:58:51 +0100

    Fix typo

--- fish/functions/getraw.fish
+++ fish/functions/getraw.fish
@@ -1,6 +1,6 @@
 function getraw -w curl -d "Retrieve single URL (including headers), output to stdout"
   if test -n "$ACCESS_TOKEN"
-    curl -sS -i --max-redirs 0 --ouptut - -H "Authorization: Bearer $ACCESS_TOKEN" $argv
+    curl -sS -i --max-redirs 0 --output - -H "Authorization: Bearer $ACCESS_TOKEN" $argv
   else
     curl -sS -i --max-redirs 0 --output - $argv
   end
