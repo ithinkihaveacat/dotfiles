bfa0db1ee241d8a6061afa9a7b5a5c05596261c8
Michael Stillwell <mjs@beebo.org>
2018-12-28 22:08:37 +0000

    fish_prompt simplifications and improvements

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,37 +1,30 @@
 function fish_prompt --description 'Write out the prompt'
 
-  if not set -q __fish_prompt_color_begin
-    set -g __fish_prompt_color_begin (set_color -i -o)
-  end
-
   if not set -q __fish_prompt_color_normal
     set -g __fish_prompt_color_normal (set_color normal)
   end
 
   if not set -q __fish_prompt_color_hostname
-    set -g __fish_prompt_color_hostname (set_color -u $fish_color_hostname)
+    set -g __fish_prompt_color_hostname (set_color normal ; set_color -i -o ; set_color -u $fish_color_hostname)
   end
 
   if not set -q __fish_prompt_color_cwd
-    set -g __fish_prompt_color_cwd (set_color $fish_color_cwd)
+    set -g __fish_prompt_color_cwd (set_color normal ; set_color -i -o ; set_color $fish_color_cwd)
   end
 
   if not set -q __fish_prompt_color_git
-    set -g __fish_prompt_color_git (set_color $fish_color_git)
+    set -g __fish_prompt_color_git (set_color normal ; set_color -i -o ; set_color $fish_color_git)
   end
 
   if not set -q __fish_prompt_color_sigil
-    set -g __fish_prompt_color_sigil (set_color $fish_color_sigil)
+    set -g __fish_prompt_color_sigil (set_color normal ; set_color -i -o ; set_color $fish_color_sigil)
   end
 
   if not set -q __fish_prompt_hostname
-    # once fish 2.5 is everywhere, use prompt_hostname here
     if test -d /etc/goobuntu
       set -g __fish_prompt_hostname goobuntu
-    else if begin ; hostname | grep -q syd ; end
-      set -g __fish_prompt_hostname syd@gandi
     else
-      set -g __fish_prompt_hostname (hostname -s|cut -d \- -f 1|tr A-Z a-z)
+      set -g __fish_prompt_hostname (string split --max 1 . (string lower $hostname))[1]
     end
   end
 
@@ -49,6 +42,6 @@ function fish_prompt --description 'Write out the prompt'
     end
   end
 
-  echo -n -s "$__fish_prompt_color_begin" "$__fish_prompt_color_hostname" "$__fish_prompt_hostname" "$__fish_prompt_color_normal" ' ' "$__fish_prompt_color_cwd" (prompt_pwd) "$__fish_prompt_color_git" (__fish_git_prompt "#%s") "$__fish_prompt_color_sigil" ' $ ' "$__fish_prompt_color_normal"
+  echo -n -s "$__fish_prompt_color_hostname" "$__fish_prompt_hostname" "$__fish_prompt_color_cwd" ' ' (prompt_pwd) "$__fish_prompt_color_git" (__fish_git_prompt "#%s") "$__fish_prompt_color_sigil" ' $ ' "$__fish_prompt_color_normal"
 
 end
