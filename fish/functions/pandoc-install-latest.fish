dc8f0e00a058a5bfe0765eef499be07026b94e30
Michael Stillwell <mjs@beebo.org>
2019-01-11 16:39:50 +0000

    Add pandoc-install-latest

---
+++ fish/functions/pandoc-install-latest.fish
@@ -0,0 +1,5 @@
+function pandoc-install-latest -d "Install latest version of pandoc"
+  set -l URL (curl -sSL https://api.github.com/repos/jgm/pandoc/releases | jq -r '.[0].assets | .[].browser_download_url' | grep macOS.pkg)
+  echo "Installing $URL..."
+  sudo installer -verbose -pkg (curl -sSL $URL | psub -s .pkg) -target /
+end
