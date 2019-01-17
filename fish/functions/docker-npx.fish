44e2d515d45d3de930f1036fa50078acc5c277c1
Michael Stillwell <mjs@beebo.org>
2019-01-17 12:29:45 +0000

    Improve docs

--- fish/functions/docker-npx.fish
+++ fish/functions/docker-npx.fish
@@ -1,3 +1,5 @@
+# Might be interesting to use https://github.com/docker/compose for some of this
+
 function docker-npx --description "Run npx in Docker container"
   # So, mounting the ~/.npm cache directory makes things slower...
   command docker run -it --rm -v "$PWD":/app:delegated -w /app node npx $argv
