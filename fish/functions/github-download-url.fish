25b2ee6f3d0ba0116f2275294b0e882ebaed1646
Michael Stillwell <mjs@beebo.org>
2019-06-04 23:06:36 +0100

    Add github-download-url

---
+++ fish/functions/github-download-url.fish
@@ -0,0 +1,11 @@
+function github-download-url -d 'Returns the URL of the most recent release'
+
+  if test ( count $argv ) -eq 0
+    printf "usage: %s user/repo # e.g. %s IBM/plex\n" (status current-command) (status current-command)
+    return
+  end
+
+  # Note that multiple URLs can be returned
+  curl --fail -sSL https://api.github.com/repos/{$argv}/releases | jq -r '.[0].assets | .[].browser_download_url'
+
+end
