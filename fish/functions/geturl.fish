3d218bdb40e4cdf01487b01f394eefc923d8e7b9
Michael Stillwell <mjs@beebo.org>
2019-06-04 23:06:44 +0100

    Follow redirects

--- fish/functions/geturl.fish
+++ fish/functions/geturl.fish
@@ -1,10 +1,10 @@
 function geturl -w curl -d "Save URL to file"
   if test -n "$ACCESS_TOKEN"
-    curl -sS -H "Authorization: Bearer $ACCESS_TOKEN" --remote-name-all $argv
+    curl -sSL -H "Authorization: Bearer $ACCESS_TOKEN" --remote-name-all $argv
     if test "$status" -eq 23
       printf "error: %s\n" (status current-command)
     end
   else
-    curl -sS --remote-name-all $argv
+    curl -sSL --remote-name-all $argv
   end
 end
