8dd433601325fd5675f5af0b9cda1b1c3e215a42
Michael Stillwell <mjs@beebo.org>
2019-07-11 22:36:15 +0100

    Make node version static

--- fish/config.fish
+++ fish/config.fish
@@ -116,7 +116,7 @@ prepend_path $d
 set -x NODE_VERSIONS $HOME/.local/share/node/versions
 mkdir -p $NODE_VERSIONS
 
-prepend_path $NODE_VERSIONS/(ls $NODE_VERSIONS | sort -V | tail -1)/bin
+prepend_path (ls -d $NODE_VERSIONS/node-v10*/bin | sort -V | tail -1)
 
 # golang
 
