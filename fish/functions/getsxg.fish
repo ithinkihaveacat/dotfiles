77a43cb47ccc3dd2df08f6042d83f1c55d773d3a
Michael Stillwell <mjs@beebo.org>
2019-10-16 21:11:15 +0100

    Improve docs

--- fish/functions/getsxg.fish
+++ fish/functions/getsxg.fish
@@ -1,3 +1,3 @@
-function getsxg -w curl -d "Attempt to retrieve SXG for URL"
+function getsxg -w curl -d "Get URL as SXG"
   curl -sS --output - -H 'accept: application/signed-exchange;v=b3' -H 'amp-cache-transform: google;v="1..100"' $argv
 end
