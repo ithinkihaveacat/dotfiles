4401702e9689745a169489210df43115211ddfb6
Michael Stillwell <mjs@beebo.org>
2019-10-28 11:10:16 +0000

    Update

--- fish/functions/amp-google-cache-url.fish
+++ fish/functions/amp-google-cache-url.fish
@@ -1,4 +1,6 @@
 function amp-google-cache-url -d "Get Google AMP Cache URL"
+  # Alternative: https://www.npmjs.com/package/@ampproject/toolbox-cache-url
+
   if test ( count $argv ) -ne 1
     printf "usage: %s URL\n" (status current-command)
     return
