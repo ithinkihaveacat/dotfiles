e5b7c00b2c890637d114ea6c743372d42ee2cd83
Michael Stillwell <mjs@beebo.org>
2019-10-30 14:12:42 +0000

    rm make.*

--- fish/functions/make.hello-world-web.fish
+++
@@ -1,7 +0,0 @@
-function make.hello-world-web
-  if test ( count $argv ) -ne 1
-    echo "usage: $_ directory"
-    return
-  end
-  svn export https://github.com/ithinkihaveacat/hello-world-web/branches/master $argv[1]
-end
