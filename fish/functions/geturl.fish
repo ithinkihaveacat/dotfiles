fb7ca4d74961ac4d37aca254baca2629c0d09152
Michael Stillwell <mjs@beebo.org>
2019-11-15 13:50:54 +0000

    Fix geturl

--- fish/functions/geturl.fish
+++ fish/functions/geturl.fish
@@ -1,6 +1,6 @@
 function geturl -w curl -d "Save URL to file"
   if test -n "$ACCESS_TOKEN"
-    set -gx CURL_CMD "curl -sSL -H \"Authorization: Bearer $ACCESS_TOKEN\" --remote-name-all"" (string escape -- $argv)
+    set -gx CURL_CMD "curl -sSL -H \"Authorization: Bearer $ACCESS_TOKEN\" --remote-name-all" (string escape -- $argv)
   else
     set -gx CURL_CMD "curl -sSL --remote-name-all" (string escape -- $argv)
   end
