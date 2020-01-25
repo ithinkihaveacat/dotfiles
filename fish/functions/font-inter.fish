a0939c2ba75bf33b0c40d3c1f4d9522b56c8cc1d
Michael Stillwell <mjs@beebo.org>
2020-01-25 11:09:13 +0000

    Improve font-*

--- fish/functions/font-inter.fish
+++ fish/functions/font-inter.fish
@@ -5,13 +5,8 @@ function font-inter
   set DIR (fontdir)/inter
   set URL (github-download-url rsms/inter)
   
-  if test -d $DIR
-    echo "error: $DIR already exists"
-    return 1
-  else
-    mkdir -p $DIR
-  end
-
+  rm -rf $DIR
+  mkdir -p $DIR
   unzip -j (curl -sL $URL | psub) 'Inter/*.otf' -d $DIR
 
 end
--- fish/functions/font-source-code-pro.fish
+++ fish/functions/font-source-code-pro.fish
@@ -5,11 +5,7 @@ function font-source-code-pro
   set DIR (fontdir)/source-code-pro
   set URL 'https://codeload.github.com/adobe-fonts/source-code-pro/zip/2.030R-ro/1.050R-it'
 
-  if test -d $DIR
-    echo "error: $DIR already exists"
-    return 1
-  end
-
+  rm -rf $DIR
   mkdir -p $DIR
   unzip -j (curl -s $URL | psub) '*.otf' -d $DIR
 
