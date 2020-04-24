b68e71154c5428ca9cccbfe3d20eb911a6150237
Michael Stillwell <mjs@beebo.org>
2020-04-24 09:15:36 +0100

    Update font-source-serif-pro

--- fish/functions/font-source-serif-pro.fish
+++ fish/functions/font-source-serif-pro.fish
@@ -5,12 +5,8 @@ function font-source-serif-pro
   set DIR (fontdir)/source-serif-pro
   set URL (github-download-url adobe-fonts/source-serif-pro)
 
-  if test -d $DIR
-    echo "error: $DIR already installed"
-    return 1
-  end
-
+  rm -rf $DIR
   mkdir -p $DIR
-  unzip -j (curl -sL $URL | psub) 'OTF/*.otf' -d $DIR
+  unzip -j (curl -sL $URL | psub) '**/OTF/*.otf' -d $DIR
 
 end
