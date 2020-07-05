025fcf4b16648657feba2a20fae80a8b167f7845
Michael Stillwell <mjs@beebo.org>
2020-07-05 22:24:04 +0100

    Display compressed bytes

--- fish/functions/getbench.fish
+++ fish/functions/getbench.fish
@@ -1,4 +1,4 @@
 function getbench -w curl -d "How long does it take to retrieve a URL?"
-  set -gx CURL_CMD "curl -ksSo /dev/null -w \"dns:%{time_namelookup} tcp:%{time_connect} ssl:%{time_appconnect} ttfb:%{time_starttransfer} total:%{time_total} (%{size_download} bytes)\n\"" (string escape -- $argv)
+  set -gx CURL_CMD "curl -ksSo /dev/null --compressed -w \"dns:%{time_namelookup} tcp:%{time_connect} ssl:%{time_appconnect} ttfb:%{time_starttransfer} total:%{time_total} (%{size_download} bytes)\n\"" (string escape -- $argv)
   echo $CURL_CMD | source
 end
