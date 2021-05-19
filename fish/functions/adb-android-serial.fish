56135cf9ac67d47a951e9e3dd4bdc80eb175bda3
Michael Stillwell <mjs@beebo.org>
2021-05-19 11:33:31 +0100

    Add adb-android-serial

---
+++ fish/functions/adb-android-serial.fish
@@ -0,0 +1,13 @@
+function adb-android-serial -d "Sets ANDROID_SERIAL, the default device for adb"
+
+  if test ( count $argv ) -eq 0
+    printf "usage: %s model # e.g. %s TicWatch\n" (status current-command) (status current-command)
+	printf "\n"
+    adb devices -l | tail +2
+    return
+  end
+
+  set -g ANDROID_SERIAL (adb devices -l | tail +2 | grep -m 1 $argv[1] | awk '{ print $1 }')
+  printf "ANDROID_SERIAL=%s\n" $ANDROID_SERIAL
+
+end
