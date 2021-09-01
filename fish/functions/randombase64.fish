c3f7b7822e286275f58c9656110a623af8902551
Michael Stillwell <mjs@beebo.org>
2021-09-01 10:55:26 +0100

    Switch to non-deprecated redirection syntax

--- fish/functions/randombase64.fish
+++ fish/functions/randombase64.fish
@@ -1,5 +1,5 @@
 function randombase64 -d "Generate random base64 string"
   set -l n $argv[1]; test -z $n; and set n 16
   # https://en.wikipedia.org/wiki/Birthday_problem#Probability_table
-  dd if=/dev/urandom bs=1 count=$n ^/dev/null | base64 | tr -d "[/+]" | cut -b -$n
+  dd if=/dev/urandom bs=1 count=$n 2>/dev/null | base64 | tr -d "[/+]" | cut -b -$n
 end
