5d65d55cc3fd8c31ef268cb7b5111e1e570f81a8
Michael Stillwell <mjs@beebo.org>
2022-01-05 10:05:09 +0000

    Update

--- fish/functions/font-jetbrains-mono.fish
+++ fish/functions/font-jetbrains-mono.fish
@@ -1,7 +1,7 @@
 function font-jetbrains-mono
 
   set DIR (fontdir)/jetbrains-mono
-  set URL https://download.jetbrains.com/fonts/JetBrainsMono-1.0.0.zip
+  set URL (github-download-url JetBrains/JetBrainsMono)
 
   if test -d $DIR
     echo "error: $DIR already exists"
@@ -10,6 +10,6 @@ function font-jetbrains-mono
     mkdir -p $DIR
   end
 
-  unzip -j (curl -sL $URL | psub) '*.ttf' -d $DIR
+  unzip -j (curl -sL $URL | psub) 'fonts/variable/*.ttf' -d $DIR
 
 end
