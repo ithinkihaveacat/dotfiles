9c65598de49e6245482ddec59f4e3e2fa5170a73
Michael Stillwell <mjs@beebo.org>
2022-08-09 23:19:32 +0100

    Update font-finlandica

--- fish/functions/font-finlandica.fish
+++ fish/functions/font-finlandica.fish
@@ -1,17 +1,18 @@
 # https://toolbox.finland.fi/brand-identity-and-guidelines/finlandica-font/
 # http://web.archive.org/web/20190323232539/https://toolbox.finland.fi/brand-identity-and-guidelines/finlandica-font/
+# https://github.com/HelsinkiTypeStudio/Finlandica
 
 function font-finlandica
 
   set DIR (fontdir)/finlandica
-  set URL 'https://toolbox.finland.fi/wp-content/uploads/sites/2/2015/09/finlandica-2018.zip'
-
+  set URL 'https://api.github.com/repos/HelsinkiTypeStudio/Finlandica/zipball'
+  
   if test -d $DIR
     echo "error: $DIR already exists"
     return 1
   end
 
   mkdir -p $DIR
-  unzip -j (curl -s $URL | psub) '*/TTF/*.ttf' -x '__MACOSX/*' -d $DIR # web versions also available!
+  unzip -j (curl -sL --output - $URL | psub) '*/fonts/variable/*.ttf' -d $DIR
 
 end
