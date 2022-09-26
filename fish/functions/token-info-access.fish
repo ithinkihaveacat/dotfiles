16a64ea1fc200de162ba32be454defd66fb9944a
Michael Stillwell <mjs@beebo.org>
2022-09-26 12:33:35 +0100

    Rename token-* scripts and migrate to local server

---
+++ fish/functions/token-info-access.fish
@@ -0,0 +1,9 @@
+function token-info-access -d "Get information about a Google access token"
+    if test ( count $argv ) -eq 0
+        printf "usage: %s access_token\n" (status current-command)
+        return
+    end
+    set url "https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=$argv[1]"
+    echo "# $url"
+    curl -sS $url
+end
---
+++ fish/functions/token-info-id.fish
@@ -0,0 +1,9 @@
+function token-info-id -d "Get information about a Google id token"
+    if test ( count $argv ) -eq 0
+        printf "usage: %s id_token" (status current-command)
+        return
+    end
+    set url "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=$argv[1]"
+    echo "# $url"
+    curl -sS $url
+end
