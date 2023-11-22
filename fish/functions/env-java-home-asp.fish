7e074ca0e7970fde77ce9ec2b878fd109fdb1e2f
Michael Stillwell <mjs@beebo.org>
2023-11-22 21:25:38 +0000

    Updates

---
+++ fish/functions/env-java-home-asp.fish
@@ -0,0 +1,4 @@
+function env-java-home-asp
+  # Example usage: (env-java-home-asp) ./gradlew app:dependencies
+  echo "env JAVA_HOME=/Applications/Android\ Studio\ Preview.app/Contents/jbr/Contents/Home"
+end
