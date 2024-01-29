c9f7312b15178ead3dd683c46af1b52057f79c33
Michael Stillwell <mjs@beebo.org>
2024-01-29 12:19:07 +0000

    Add font-iA

---
+++ fish/functions/font-iA.fish
@@ -0,0 +1,12 @@
+# https://monaspace.githubnext.com
+
+function font-iA
+
+  set DIR (fontdir)/iA
+  set URL 'https://api.github.com/repos/iaolo/iA-Fonts/zipball'
+
+  rm -rf $DIR
+  mkdir -p $DIR
+  unzip -j (curl -sL $URL | psub) '**/Static/*.ttf' -d $DIR
+
+end
