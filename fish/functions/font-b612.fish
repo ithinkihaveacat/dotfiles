fce7e6da69b5729a9cd46b90284c7893038dbed5
Michael Stillwell <mjs@beebo.org>
2024-02-01 00:17:48 +0000

    Add font-b612

---
+++ fish/functions/font-b612.fish
@@ -0,0 +1,12 @@
+# https://b612-font.com/
+
+function font-b612
+
+  set DIR (fontdir)/b612
+  set URL 'https://api.github.com/repos/polarsys/b612/zipball'
+
+  rm -rf $DIR
+  mkdir -p $DIR
+  unzip -j (curl -sL $URL | psub) '**/fonts/ttf/*.ttf' -d $DIR
+
+end
