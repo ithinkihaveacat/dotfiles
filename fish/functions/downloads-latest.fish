0c37125ca49ffb7617723a3bdd96a268c2826c6d
Michael Stillwell <mjs@beebo.org>
2024-05-09 15:26:56 +0100

    Improve robustness of *-latest

--- fish/functions/downloads-latest.fish
+++ fish/functions/downloads-latest.fish
@@ -1,5 +1,5 @@
 function downloads-latest -d 'echos filename of newest file in ~/Downloads'
 
-  ls -trd $HOME/Downloads/* | tail -1
+  find "$HOME/Downloads" -type f -exec stat -f "%m %N" {} + | sort -n -r | head -n 1 | cut -d" " -f2-
 
 end
