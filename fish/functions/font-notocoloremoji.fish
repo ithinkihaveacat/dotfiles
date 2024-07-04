145b170949dcfc742ecdf96632c326cffd089d5d
Michael Stillwell <mjs@beebo.org>
2024-07-04 17:52:32 +0100

    Add font-* scripts

---
+++ fish/functions/font-notocoloremoji.fish
@@ -0,0 +1,16 @@
+# https://github.com/googlefonts/noto-emoji
+
+function font-notocoloremoji
+
+  set DIR (fontdir)/notocoloremoji
+  set URL 'https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji-noflags.ttf'
+
+  if test -d $DIR
+    echo "error: $DIR already exists"
+    return 1
+  end
+
+  mkdir -p $DIR
+  curl -sL --output - $URL > $DIR/NotoColorEmoji.ttf
+
+end
