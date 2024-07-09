a7da8cf2b94fafebf730267f0d160cb7af56c962
Michael Stillwell <mjs@beebo.org>
2024-07-09 22:07:14 +0100

    Add adb commands

---
+++ fish/functions/adb-settings-diff.fish
@@ -0,0 +1,7 @@
+function adb-settings-diff
+
+  set -l SETTINGS adb exec-out dumpsys settings
+
+  vimdiff ($SETTINGS | psub) (read -p "echo 'Make change and press enter (:qa to exit vimdiff!) > '" ; and $SETTINGS | psub)
+
+end
