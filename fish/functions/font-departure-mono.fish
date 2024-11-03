60c787d1122a0b68d861ae813eadb56f49462164
Michael Stillwell <mjs@beebo.org>
2024-11-03 23:05:24 +0000

    Pixelated font

---
+++ fish/functions/font-departure-mono.fish
@@ -0,0 +1,16 @@
+# https://github.com/rektdeckard/departure-mono/releases
+
+function font-departure-mono
+
+  set DIR (fontdir)/departure-mono
+  set URL (github-download-url rektdeckard/departure-mono)
+
+  if test -d $DIR
+    echo "error: $DIR already exists"
+    return 1
+  end
+
+  mkdir -p $DIR
+  unzip -j (curl -sL $URL | psub) '*.otf' -d $DIR
+
+end
