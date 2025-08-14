5bbdf404fbc9d7a640ab19c0b06a938719523b8d
Michael Stillwell <mjs@beebo.org>
2025-08-14 22:22:52 +0100

    Remove [cite]

--- fish/functions/pbclean.fish
+++ fish/functions/pbclean.fish
@@ -1,3 +1,3 @@
 function pbclean
-    pbpaste | grep -v "data:image/png;base64," | string replace --all (printf '\u00a0') ' ' | pbcopy
+    pbpaste | grep -v "data:image/png;base64," | string replace --all (printf '\u00a0') ' ' | string replace -a -r '\[cite[^]]*]' '' | pbcopy
 end
