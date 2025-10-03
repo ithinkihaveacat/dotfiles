ec47d9433a0fe0325a63f3b01ca868592f628984
Michael Stillwell <mjs@beebo.org>
2025-10-03 12:31:05 +0100

    Remove ---

--- fish/functions/pbclean.fish
+++ fish/functions/pbclean.fish
@@ -1,3 +1,3 @@
 function pbclean
-    pbpaste | grep -v "data:image/png;base64," | string replace --all (printf '\u00a0') ' ' | string replace -a -r '\[cite[^]]*]' '' | pbcopy
+    pbpaste | grep -v "data:image/png;base64," | string replace --all (printf '\u00a0') ' ' | string replace --all -r '\[cite[^]]*]' '' | string replace --all -- '---' '' | pbcopy
 end
