9c0d05e6530343861dc21ea2c9cfceae861cf5b0
Michael Stillwell <mjs@beebo.org>
2025-11-16 22:48:23 +0000

    fzf unused

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,4 @@
 function fish_user_key_bindings
   bind \033f nextd-or-forward-word
   bind \033b prevd-or-backward-word
-  if type -q brew
-    and type -q fzf
-	source (brew --prefix)/opt/fzf/shell/key-bindings.fish
-	fzf_key_bindings
-  end
 end
