1e64e6e2ba9358349749738a0539466e3cb85f76
Michael Stillwell <mjs@beebo.org>
2025-12-09 21:51:07 +0000

    Set TMPDIR if unavailable

--- fish/functions/gemini.fish
+++ fish/functions/gemini.fish
@@ -1,4 +1,5 @@
 # Wrapper for gemini that automatically includes TMPDIR in context
 function gemini --description 'Run gemini with TMPDIR automatically included in context'
+    set -lx TMPDIR (test -n "$TMPDIR"; and echo $TMPDIR; or mktemp -d)
     command gemini --include-directories $TMPDIR $argv
 end
