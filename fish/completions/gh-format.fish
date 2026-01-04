c6378d79533ccbbd87d8e4a0b16efb086aa3a203
Michael Stillwell <mjs@beebo.org>
2026-01-04 16:15:30 +0000

    gh-format: improve usage display and add creation date
    - Display usage instructions when no arguments are provided and running in a terminal.
    - Include the issue/PR creation date and author in the output.

---
+++ fish/completions/gh-format.fish
@@ -0,0 +1,6 @@
+# Fish completions for gh-format
+
+# Complete options
+complete -c gh-format -l comments -d 'Include all discussion comments'
+complete -c gh-format -l output -r -F -d 'Write output to file'
+complete -c gh-format -s h -l help -d 'Display help'
