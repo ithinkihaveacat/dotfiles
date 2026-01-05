aa74d3d37b8b6848d0147259e1b5be5eedefecd3
Michael Stillwell <mjs@beebo.org>
2026-01-05 21:24:19 +0000

    Add --type argument to adb-tile-add
    Add support for specifying the tile type (FULLSCREEN, LARGE, SMALL) when
    adding a tile. This maps to the '--ei type' extra intent and is applicable
    to tiles supporting the 'BIND_WIDGET_PROVIDER' intent-filter (e.g.,
    Glance-based tiles).
    
    Also update the help usage text and Fish shell completions to include the
    new option.

--- fish/completions/adb-tile-add.fish
+++ fish/completions/adb-tile-add.fish
@@ -1,4 +1,5 @@
 # completions for adb-tile-add
 complete -c adb-tile-add -f
 complete -c adb-tile-add -l no-show -d 'Do not show the tile after adding it'
+complete -c adb-tile-add -l type -d 'Tile type' -r -a 'FULLSCREEN LARGE SMALL'
 complete -c adb-tile-add -s h -l help -d 'Display help'
