5a7f04bfc27be3c41ba6ae2b4d1e9ff9fb3d364f
Michael Stillwell <mjs@beebo.org>
2026-01-14 10:38:46 +0000

    Add adb-activities to list and tag activities
    This new script lists activities on the connected device and tags them by category (Launcher, Home, TV, Settings). This is similar to adb-tiles but for activities, allowing for quick identification of application entry points.
    
    Includes Fish completion.

---
+++ fish/completions/adb-activities.fish
@@ -0,0 +1,8 @@
+# Fish completion for adb-activities
+
+complete -c adb-activities -f
+complete -c adb-activities -s h -l help -d "Display help message"
+complete -c adb-activities -l launcher-only -d "List only launcher activities"
+complete -c adb-activities -l home-only -d "List only home/launcher app activities"
+complete -c adb-activities -l tv-only -d "List only TV/Leanback activities"
+complete -c adb-activities -l settings-only -d "List only settings activities"
