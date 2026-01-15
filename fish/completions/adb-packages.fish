6d2cbfc4bf31df150d1d1bbadb2e0c8b0086e721
Michael Stillwell <mjs@beebo.org>
2026-01-15 01:02:47 +0000

    adb-packages: Add --all switch to list all packages
    Added support for listing all packages (including system apps) via the
    --all switch. Default behavior remains listing only third-party
    packages.
    
    Also added:
    - --help usage information
    - Device connection check
    - Fish shell completions

---
+++ fish/completions/adb-packages.fish
@@ -0,0 +1,5 @@
+# Fish completion for adb-packages
+
+complete -c adb-packages -f
+complete -c adb-packages -s h -l help -d "Display help message"
+complete -c adb-packages -l all -d "List all packages (including system apps)"
