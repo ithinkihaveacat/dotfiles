653ea309d44636bb06f22fb06d3036ef343b3142
Michael Stillwell <mjs@beebo.org>
2026-01-15 20:04:24 +0000

    fish/functions/pbclean.fish: filter out markdown data-uri images
    Updates the pbclean function to remove lines containing markdown reference-style data-uri images (e.g. '[image1]: <data:image/...').
    
    This is done by combining the existing sed substitution for quoted data-uris with a new delete command for the markdown pattern.

--- fish/functions/pbclean.fish
+++ fish/functions/pbclean.fish
@@ -1,3 +1,3 @@
 function pbclean
-    pbpaste | sed -E 's/"data:[^"]*"/""/g' | string replace --all (printf '\u00a0') ' ' | string replace --all -r '\[cite[^]]*]' '' | string replace --all -- --- '' | pbcopy
+    pbpaste | sed -E -e '/^\[image[0-9]+\]: <data:image\//d' -e 's/"data:[^"]*"/""/g' | string replace --all (printf '\u00a0') ' ' | string replace --all -r '\[cite[^]]*]' '' | string replace --all -- --- '' | pbcopy
 end
