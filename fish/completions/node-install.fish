a19a3ad72301769a0d836859cd97778b31787aa5
Michael Stillwell <mjs@beebo.org>
2026-01-29 11:36:17 +0000

    Add completion for node-install --installed

--- fish/completions/node-install.fish
+++ fish/completions/node-install.fish
@@ -1,3 +1,4 @@
 # completions for node-install
 complete -c node-install -s h -l help -d 'Display help'
-complete -c node-install -s l -l list -d 'List available Node.js versions'
+complete -c node-install -s l -l list -d 'List available remote Node.js versions'
+complete -c node-install -s i -l installed -d 'List installed Node.js versions'
