8014bed5cf6fc1f93431c1f6bb6ae59a3e8cc693
Michael Stillwell <mjs@beebo.org>
2026-02-10 17:59:45 +0000

    Fix tab completion for 'e' command
    Remove the '-f' (no files) flag from the completion definition for the 'e'
    function.
    
    This allows the 'e' command to tab-complete standard file paths in the
    current directory, while still offering completions for function names and
    scripts in the bin directory.

--- fish/completions/e.fish
+++ fish/completions/e.fish
@@ -1,6 +1,6 @@
 # Complete with fish function names
-complete -c e -f -a "(functions -n)"
+complete -c e -a "(functions -n)"
 
 # Complete with scripts from dotfiles bin directory
 # (Filtering text files across all of PATH would be too slow)
-complete -c e -f -a "(printf '%s\n' $HOME/.dotfiles/bin/* 2>/dev/null | xargs -n1 basename 2>/dev/null)"
+complete -c e -a "(printf '%s\n' $HOME/.dotfiles/bin/* 2>/dev/null | xargs -n1 basename 2>/dev/null)"
