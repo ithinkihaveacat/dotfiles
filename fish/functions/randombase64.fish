36104c946b879d7dceeaf11a6116c2336f7acd46
Michael Stillwell <mjs@beebo.org>
2026-04-10 14:24:02 +0100

    refactor(fish): use openssl for random strings
    Migrate randomhex and randombase64 to use 'openssl rand' instead of
    piping from /dev/urandom into md5sum and base64.
    
    Using openssl improves portability by removing the dependency on GNU
    md5sum, which is not installed by default on macOS. It also removes
    arbitrary length limitations present in the previous md5sum-based
    implementation and is generally more robust for generating
    cryptographically secure random bytes.

--- fish/functions/randombase64.fish
+++ fish/functions/randombase64.fish
@@ -2,5 +2,5 @@ function randombase64 -d "Generate random base64 string"
     set -l n $argv[1]
     test -z $n; and set n 16
     # https://en.wikipedia.org/wiki/Birthday_problem#Probability_table
-    dd if=/dev/urandom bs=1 count=$n 2>/dev/null | base64 | tr -d "[/+]" | cut -b -$n
+    openssl rand -base64 $n | tr -d "[/+=\n]" | cut -c -$n
 end
