e42cdc0bf772cfeadd253101f2cf6f9370031d7c
Michael Stillwell <mjs@beebo.org>
2026-05-24 17:56:01 +0100

    fix(prompt): resolve errors in .git directory
    Suppress stderr when calling git rev-parse --show-toplevel and verify
    that its output is non-empty before running basename. This prevents
    errors when entering git directories without a working tree (such as
    the .git directory itself).
    
    Also optimize prompt rendering by replacing the external basename
    binary call with the Fish built-in string replace, and explicitly
    use the global scope (-g) flag for configuration variables.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,8 +1,8 @@
 # https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_git_prompt.fish
 
 # PWD Settings
-set fish_prompt_pwd_dir_length 1
-set fish_prompt_pwd_full_dirs 3
+set -g fish_prompt_pwd_dir_length 1
+set -g fish_prompt_pwd_full_dirs 3
 
 # Transient prompts (hide right prompt on non-active lines) requires fish 4.1+
 # fish 4.0.x does not support --final-rendering flag
@@ -11,29 +11,18 @@ set -l major $version_parts[1]
 set -l minor $version_parts[2]
 
 if test $major -gt 4; or test $major -eq 4 -a $minor -ge 1
-    set fish_transient_prompt 1
+    set -g fish_transient_prompt 1
 end
 
 # Git Logic
-set __fish_git_prompt_showupstream auto
-set __fish_git_prompt_showstashstate yes
-set __fish_git_prompt_show_informative_status yes
+set -g __fish_git_prompt_showupstream auto
+set -g __fish_git_prompt_showstashstate yes
+set -g __fish_git_prompt_show_informative_status yes
 
 # Git Characters (Customizing the look)
-set __fish_git_prompt_char_stateseparator ' '
-set __fish_git_prompt_char_cleanstate "✔"
-set __fish_git_prompt_color_cleanstate --bold white
-#set __fish_git_prompt_char_dirtystate '*'     # Overrides default ✚
-#set __fish_git_prompt_char_stagedstate '⇢'    # Overrides default ●
-
-# Git Characters
-#set __fish_git_prompt_char_dirtystate '*'
-#set __fish_git_prompt_char_stagedstate '⇢'
-#set __fish_git_prompt_char_upstream_prefix ' '
-#set __fish_git_prompt_char_upstream_equal ''
-#set __fish_git_prompt_char_upstream_ahead '⇡'
-#set __fish_git_prompt_char_upstream_behind '⇣'
-#set __fish_git_prompt_char_upstream_diverged '⇡⇣'
+set -g __fish_git_prompt_char_stateseparator ' '
+set -g __fish_git_prompt_char_cleanstate "✔"
+set -g __fish_git_prompt_color_cleanstate --bold white
 
 function fish_prompt --description 'Write out the prompt'
     set -l last_status $status
@@ -49,13 +38,10 @@ function fish_prompt --description 'Write out the prompt'
     echo -n "\$ "
 
     set_color normal
-
 end
 
 function fish_right_prompt --description 'Write out the right prompt'
-
     if not contains -- --final-rendering $argv
-
         if is_remote
             set_color $fish_color_ssh
             printf "@%s " (prompt_hostname)
@@ -63,13 +49,14 @@ function fish_right_prompt --description 'Write out the right prompt'
         end
 
         if __fish_is_git_repository
-            set_color yellow
-            basename (git rev-parse --show-toplevel)
-            set_color normal
-            echo -n " "
+            set -l toplevel (git rev-parse --show-toplevel 2>/dev/null)
+            if test -n "$toplevel"
+                set_color yellow
+                string replace -r '^.*/' '' $toplevel
+                set_color normal
+                echo -n " "
+            end
             __fish_git_prompt "%s"
         end
-
     end
-
 end
